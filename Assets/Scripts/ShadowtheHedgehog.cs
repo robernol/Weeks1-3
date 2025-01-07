cdfbfa19e1ae4b12f0bf703b97b822bd9ff744ff
robernol <robernol@sheridancollege.ca>
2025-01-07 18:24:51 -0500

    fixed shadow :^]

--- Assets/Scripts/ShadowtheHedgehog.cs
+++ Assets/Scripts/ShadowtheHedgehog.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class ShadowtheHedgehog : MonoBehaviour
 {
-    float speed = 0.02f;
+    float speed = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +17,18 @@ public class ShadowtheHedgehog : MonoBehaviour
     {
         Vector2 pos = transform.position;
         pos.x += speed;
-        transform.position = pos;
 
+        Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+        Vector2 screenSizeInTheWorld = new Vector2();
+        screenSizeInTheWorld = Camera.main.ScreenToWorldPoint(screenSize);
 
-        if ((pos.x >= 10) || (pos.x <= -10))
+        Vector2 screenZeroInTheWorld = Camera.main.ScreenToWorldPoint(Vector2.zero);
+
+        if ((pos.x >= screenSizeInTheWorld.x) || (pos.x <= screenZeroInTheWorld.x))
                 {
             speed *= -1;
         }
+
+        transform.position = pos;
     }
 }
